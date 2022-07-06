a260c641d720679fad1c13542b6db83269f3417e
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-07-06 18:07:34 -0400

    Created handlePerformSearch function in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import apiKey from '../config';
 
 import SearchForm from './SearchForm';
 import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
 
 function App() {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        
+    });
+
+    function handlePerformSearch(searchInput) {
+        const endpoint = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchInput}&per_page=24&format=json&nojsoncallback=1`;
+        fetch(endpoint)
+            .then(response => response.json)
+            .then(jsonData => {
+                setPhotos(jsonData.photos.photo);
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error);
+            });
+    }
+
     return (
         <div className="container">
-            <SearchForm />
+            <SearchForm performSearch={handlePerformSearch}/>
             <Nav />
             <PhotoContainer />
         </div>

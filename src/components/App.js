631c98d37af820096442b2c53f9361b8f4a4b0a5
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-07-06 16:49:17 -0400

    Edit index.js and build App.js

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import SearchForm from './SearchForm';
+import MainNav from './MainNav';
+import PhotoContainer from './PhotoContainer';
+
+function App() {
+    return (
+        <div class="container">
+            <SearchForm />
+            <MainNav />
+            <PhotoContainer />
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './css/index.css';
 
-const container = document.querySelector('.container');
+const container = document.getElementByID('root');
 const root = ReactDOM.createRoot(container);
 root.render(<App />);

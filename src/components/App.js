dc334cf9aa368313df5f87cfa1e5da1521046fa3
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-07-06 17:26:59 -0400

    Edit App and NavItem

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import SearchForm from './SearchForm';
-import MainNav from './MainNav';
+import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
 
 function App() {
     return (
         <div className="container">
             <SearchForm />
-            {/* <MainNav />
+            {/* <Nav />
             <PhotoContainer /> */}
         </div>
     );
---
+++ src/components/NavItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NavItem() {
+    return (
+        <li><a href='#'>Cats</a></li>
+    );
+}
+
+export default NavItem;

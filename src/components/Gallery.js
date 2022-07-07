454533254ccb958d9cd4a3239535882303b966df
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-07-07 00:06:42 -0400

    Add comments to Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,13 +4,16 @@ import apiKey from '../config';
 import PhotoContainer from './PhotoContainer';
 
 function Gallery() {
+    // Initiate `photos` and `isLoading` states
     const [photos, setPhotos] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
+    // Get topic from params (if provided through search) or from location object (if links are used and param doesn't exist)
     const location = useLocation();
     let {topic} = useParams();
     topic = topic ? topic : location.pathname.slice(1);
 
+    // Use `topic` to fetch photos and update `photos` and `isLoading` states  
     useEffect(() => {
         const endpoint = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${topic}&per_page=24&format=json&nojsoncallback=1`;
         fetch(endpoint)

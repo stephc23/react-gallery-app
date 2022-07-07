7c63b1b4da191aea667de997e2c323f8d1135401
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-07-06 23:59:44 -0400

    Rename NoResults and PageNotFound components

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PageNotFound() {
+    return (
+        <div className="page-not-found">
+            <h3>404 Error</h3>
+            <p>This page does not exist.</p>
+        </div>
+    );
+}
+
+export default PageNotFound;
